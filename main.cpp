f666c0f1b66ec8a3ca11cbacfc085332b7ea174a
Derek <demeng.chen@mail.utoronto.ca>
2022-07-13 00:23:16 -0400

    Initial push which contains file strectures, no code written.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*
+    Wave function collapse, procedural generation of images and such.
+*/
+
+#include "./src/utilities.h"
+
+
+int main(){
+    
+
+    return 0;
+}
---
+++ src/utilities.h
@@ -0,0 +1,3 @@
+#pragma once
+#include "lodepng.h"
+#include "structure.h"
